05cafc3fe5bacbd4fae45ade0f6fada7ad6bdce3
Jacob <jhowell45@googlemail.com>
2026-04-09 01:08:46 +0100

    add tasks to the workers

--- backend/app/worker.py
+++ backend/app/worker.py
@@ -4,7 +4,7 @@ from app.deps.settings import get_settings
 
 settings = get_settings()
 
-celery = Celery(__file__, broker=str(settings.CELERY_URI))
+celery = Celery(__file__, broker=str(settings.CELERY_URI), include=["app.jobs"])
 
 if __name__ == "__main__":
     celery.start()
